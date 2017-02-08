cc6025db1d4f47abfaf586ca16fcd25ac9deb73f
Dion <dnwj1994@gmail.com>
2017-02-08 23:09:32 +0800

    added the checkIfStorageFileExist() method into save(Addressbook addressbook) method in StorageFile.java

--- src/seedu/addressbook/storage/StorageFile.java
+++ src/seedu/addressbook/storage/StorageFile.java
@@ -108,6 +108,7 @@ public class StorageFile {
         /* Note: Note the 'try with resource' statement below.
          * More info: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
          */
+    	checkIfStorageFileExist();
         try (final Writer fileWriter =
                      new BufferedWriter(new FileWriter(path.toFile()))) {
 
