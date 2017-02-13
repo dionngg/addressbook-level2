57dd61718c3c0e6e250d1b00f26f8cb0985cd6b2
Dion <dnwj1994@gmail.com>
2017-02-13 10:25:36 +0800

    implemented interface Printable to Person.java to add concatanated version of getPrintableString() method to the class

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -8,7 +8,7 @@ import java.util.Objects;
  * Represents a Person in the address book.
  * Guarantees: details are present and not null, field values are validated.
  */
-public class Person implements ReadOnlyPerson {
+public class Person implements ReadOnlyPerson, Printable {
 
     private Name name;
     private Phone phone;
@@ -83,5 +83,10 @@ public class Person implements ReadOnlyPerson {
     public String toString() {
         return getAsTextShowAll();
     }
+
+	@Override
+	public String getPrintableString() {
+		return phone.getPrintableString()+ ", " + email.getPrintableString() + ", " + address.getPrintableString();
+	}
 
 }
